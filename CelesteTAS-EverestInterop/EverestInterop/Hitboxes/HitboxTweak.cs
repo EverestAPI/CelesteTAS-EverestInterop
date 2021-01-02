4444d3150da23a89d2eed38957860fd69fbd7f50
DemoJameson <demojameson@gmail.com>
2021-01-02 17:19:20 +0800

    Avoid redraw corners hitboxes.

--- CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxTweak.cs
+++ CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxTweak.cs
@@ -9,6 +9,7 @@ namespace TAS.EverestInterop.Hitboxes {
     public class HitboxTweak {
         public static HitboxTweak instance;
         private static CelesteTASModuleSettings Settings => CelesteTASModule.Settings;
+        private static readonly FieldInfo RectFieldInfo = typeof(Draw).GetField("rect", BindingFlags.Static | BindingFlags.NonPublic);
 
         private static readonly List<Type> UselessTypes = new List<Type> {
             typeof(ClutterBlockBase),
@@ -28,6 +29,7 @@ namespace TAS.EverestInterop.Hitboxes {
             On.Monocle.Entity.DebugRender += HideHitbox;
             On.Monocle.Grid.Render += CombineHitbox;
             On.Monocle.Hitbox.Render += ModHitbox;
+            On.Monocle.Draw.HollowRect_float_float_float_float_Color += AvoidRedrawCorners;
             HitboxTriggerSpikes.Load();
         }
 
@@ -35,6 +37,7 @@ namespace TAS.EverestInterop.Hitboxes {
             On.Monocle.Entity.DebugRender -= HideHitbox;
             On.Monocle.Grid.Render -= CombineHitbox;
             On.Monocle.Hitbox.Render -= ModHitbox;
+            On.Monocle.Draw.HollowRect_float_float_float_float_Color -= AvoidRedrawCorners;
             HitboxTriggerSpikes.Unload();
         }
 
@@ -77,56 +80,108 @@ namespace TAS.EverestInterop.Hitboxes {
             }
         }
 
+        private static void AvoidRedrawCorners(On.Monocle.Draw.orig_HollowRect_float_float_float_float_Color orig, float x,
+            float y, float width, float height, Color color) {
+            if (!CelesteTASModule.Settings.SimplifiedHitboxes) {
+                orig(x, y, width, height, color);
+                return;
+            }
+
+            var rect = (Rectangle) RectFieldInfo.GetValue(null);
+            rect.X = (int) x;
+            rect.Y = (int) y;
+            rect.Width = (int) width;
+            rect.Height = 1;
+            Draw.SpriteBatch.Draw(Draw.Pixel.Texture.Texture_Safe, rect, Draw.Pixel.ClipRect, color);
+
+            rect.Y += (int) height - 1;
+            Draw.SpriteBatch.Draw(Draw.Pixel.Texture.Texture_Safe, rect, Draw.Pixel.ClipRect, color);
+
+            rect.Y -= (int) height - 2;
+            rect.Width = 1;
+            rect.Height = (int) height - 2;
+            Draw.SpriteBatch.Draw(Draw.Pixel.Texture.Texture_Safe, rect, Draw.Pixel.ClipRect, color);
+
+            rect.X += (int) width - 1;
+            Draw.SpriteBatch.Draw(Draw.Pixel.Texture.Texture_Safe, rect, Draw.Pixel.ClipRect, color);
+        }
+
         private static void DrawCombineHollowRect(Grid grid, Color color, int x, int y, int left, int right, int top, int bottom) {
             float topLeftX = grid.AbsoluteLeft + x * grid.CellWidth;
             float topLeftY = grid.AbsoluteTop + y * grid.CellHeight;
-            Vector2 vector2Width = Vector2.UnitX * grid.CellWidth;
-            Vector2 vector2Height = Vector2.UnitY * grid.CellHeight;
+            Vector2 width = Vector2.UnitX * grid.CellWidth;
+            Vector2 height = Vector2.UnitY * grid.CellHeight;
 
             Vector2 topLeft = new Vector2(topLeftX, topLeftY);
-            Vector2 topRight = topLeft + vector2Width;
-            Vector2 bottomLeft = topLeft + vector2Height;
-            Vector2 bottomRight = topRight + vector2Height;
+            Vector2 topRight = topLeft + width;
+            Vector2 bottomLeft = topLeft + height;
+            Vector2 bottomRight = topRight + height;
+
+            bool drawnLeft = false, drawnRight = false, drawnTop = false, drawnBottom = false;
 
             VirtualMap<bool> data = grid.Data;
 
             if (data[x, y]) {
                 // left
                 if (x != left && !data[x - 1, y]) {
-                    Draw.Line(topLeft + Vector2.UnitX, bottomLeft + Vector2.UnitX, color);
+                    Draw.Line(topLeft + Vector2.One, bottomLeft + Vector2.UnitX - Vector2.UnitY, color);
+                    drawnLeft = true;
                 }
 
                 // right
                 if (x == right || x + 1 <= right && !data[x + 1, y]) {
-                    Draw.Line(topRight, bottomRight, color);
+                    Draw.Line(topRight + Vector2.UnitY, bottomRight - Vector2.UnitY, color);
+                    drawnRight = true;
                 }
 
                 // top
                 if (y != top && !data[x, y - 1]) {
-                    Draw.Line(topLeft, topRight, color);
+                    Draw.Line(topLeft + Vector2.UnitX, topRight - Vector2.UnitX, color);
+                    drawnTop = true;
                 }
 
                 // bottom
                 if (y == bottom || y + 1 <= bottom && !data[x, y + 1]) {
-                    Draw.Line(bottomLeft - Vector2.UnitY, bottomRight - Vector2.UnitY, color);
+                    Draw.Line(bottomLeft - Vector2.UnitY + Vector2.UnitX, bottomRight - Vector2.One, color);
+                    drawnBottom = true;
+                }
+
+                // top left point
+                if (drawnTop || drawnLeft) {
+                    Draw.Point(topLeft, color);
+                }
+
+                // top right point
+                if (drawnTop || drawnRight) {
+                    Draw.Point(topRight - Vector2.UnitX, color);
+                }
+
+                // bottom left point
+                if (drawnBottom || drawnLeft) {
+                    Draw.Point(bottomLeft - Vector2.UnitY, color);
+                }
+
+                // bottom right point
+                if (drawnBottom || drawnRight) {
+                    Draw.Point(bottomRight - Vector2.One, color);
                 }
             } else {
-                // top left point
+                // inner hollow top left point
                 if (x - 1 >= left && y - 1 >= top && data[x - 1, y - 1] && data[x - 1, y] && data[x, y - 1]) {
                     Draw.Point(topLeft - Vector2.One, color);
                 }
 
-                // top right point
+                // inner hollow top right point
                 if (x + 1 <= right && y - 1 >= top && data[x + 1, y - 1] && data[x + 1, y] && data[x, y - 1]) {
                     Draw.Point(topRight - Vector2.UnitY, color);
                 }
 
-                // bottom left point
+                // inner hollow bottom left point
                 if (x - 1 >= left && y + 1 <= bottom && data[x - 1, y + 1] && data[x - 1, y] && data[x, y + 1]) {
                     Draw.Point(bottomLeft - Vector2.UnitX, color);
                 }
 
-                // bottom right point
+                // inner hollow bottom right point
                 if (x + 1 <= right && y + 1 >= top && data[x + 1, y + 1] && data[x + 1, y] && data[x, y + 1]) {
                     Draw.Point(bottomRight, color);
                 }
