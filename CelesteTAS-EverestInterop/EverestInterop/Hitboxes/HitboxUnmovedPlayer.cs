d9f736eefd5ee9e393559235a6c08c71f57ae6d9
DemoJameson <demojameson@gmail.com>
2021-01-08 19:58:23 +0800

    Fix unmoved hitbox not work when player speed is zero

--- CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxUnmovedPlayer.cs
+++ CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxUnmovedPlayer.cs
@@ -6,46 +6,55 @@ using Monocle;
 namespace TAS.EverestInterop.Hitboxes {
     public static class HitboxUnmovedPlayer {
         private static readonly FieldInfo PlayerHurtbox = typeof(Player).GetPrivateField("hurtbox");
-        private static CelesteTASModuleSettings Settings => CelesteTASModule.Settings;
         private static readonly Color hitboxColor = Color.Red.Invert() * 0.7f;
         private static readonly Color hurtboxColor = Color.Lime.Invert() * 0.7f;
+        private static CelesteTASModuleSettings Settings => CelesteTASModule.Settings;
 
         public static void Load() {
+            On.Celeste.Player.Update += PlayerOnUpdate;
             On.Monocle.Hitbox.Render += HitboxOnRender;
         }
 
         public static void Unload() {
+            On.Celeste.Player.Update -= PlayerOnUpdate;
             On.Monocle.Hitbox.Render -= HitboxOnRender;
         }
 
+        private static void PlayerOnUpdate(On.Celeste.Player.orig_Update orig, Player self) {
+            orig(self);
+            self.SaveLastPosition();
+        }
+
         private static void HitboxOnRender(On.Monocle.Hitbox.orig_Render orig, Hitbox self, Camera camera, Color color) {
-            if (!(self.Entity is Player player) || !Settings.ShowHitboxes || !Settings.ShowUnmovedPlayerHitbox) {
+            if (!(self.Entity is Player player) || !Settings.ShowHitboxes || !Settings.ShowUnmovedPlayerHitbox
+                || player.LoadLastPosition() == null
+                || player.LoadLastPosition().Value == player.Position
+                || player.Scene is Level level && level.Transitioning
+            ) {
                 orig(self, camera, color);
                 return;
             }
 
-            Vector2 offset = (player.ExactPosition - player.PreviousPosition - player.Speed * Engine.DeltaTime).Round();
+            // unmoved hitbox
+            DrawAssistedHitbox(orig, self, camera, player, player.LoadLastPosition().Value);
 
-            if (offset == Vector2.Zero) {
-                orig(self, camera, color);
-                return;
-            }
+            orig(self, camera, color);
+        }
 
-            player.Position -= offset;
-
-            // hitbox
-            orig(self, camera, hitboxColor);
-
-            // hurtbox
+        private static void DrawAssistedHitbox(On.Monocle.Hitbox.orig_Render orig, Hitbox self, Camera camera, Player player,
+            Vector2 hitboxPosition) {
             Collider origCollider = player.Collider;
             Collider hurtbox = (Collider) PlayerHurtbox.GetValue(player);
+            Vector2 origPosition = player.Position;
+
+            player.Position = hitboxPosition;
+
+            orig(self, camera, hitboxColor);
             player.Collider = hurtbox;
             Draw.HollowRect(hurtbox, hurtboxColor);
             player.Collider = origCollider;
 
-            player.Position += offset;
-
-            orig(self, camera, color);
+            player.Position = origPosition;
         }
     }
 }
