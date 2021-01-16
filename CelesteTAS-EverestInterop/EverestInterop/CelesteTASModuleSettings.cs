232dbc923405f9d1b72e5f66cdaaafbe81983470
DemoJameson <demojameson@gmail.com>
2021-01-17 00:01:26 +0800

    Show hitboxes when hide gameplay

--- CelesteTAS-EverestInterop/EverestInterop/CelesteTASModuleSettings.cs
+++ CelesteTAS-EverestInterop/EverestInterop/CelesteTASModuleSettings.cs
@@ -6,106 +6,101 @@ using Microsoft.Xna.Framework;
 using TAS.EverestInterop.Hitboxes;
 
 namespace TAS.EverestInterop {
-	public class CelesteTASModuleSettings : EverestModuleSettings {
+    public class CelesteTASModuleSettings : EverestModuleSettings {
+        public bool Enabled { get; set; } = true;
 
-		public bool Enabled { get; set; } = true;
+        [DefaultButtonBinding(0, Keys.RightControl)]
+        public ButtonBinding KeyStart { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.RightControl)]
-		public ButtonBinding KeyStart { get; set; } = new ButtonBinding();
+        [DefaultButtonBinding(0, Keys.RightShift)]
+        public ButtonBinding KeyFastForward { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.RightShift)]
-		public ButtonBinding KeyFastForward { get; set; } = new ButtonBinding();
+        [DefaultButtonBinding(0, Keys.OemOpenBrackets)]
+        public ButtonBinding KeyFrameAdvance { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.OemOpenBrackets)]
-		public ButtonBinding KeyFrameAdvance { get; set; } = new ButtonBinding();
+        [DefaultButtonBinding(0, Keys.OemCloseBrackets)]
+        public ButtonBinding KeyPause { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.OemCloseBrackets)]
-		public ButtonBinding KeyPause { get; set; } = new ButtonBinding();
+        [DefaultButtonBinding(0, Keys.B)] public ButtonBinding KeyHitboxes { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.B)]
-		public ButtonBinding KeyHitboxes { get; set; } = new ButtonBinding();
+        [DefaultButtonBinding(0, Keys.N)] public ButtonBinding KeyGraphics { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.N)]
-		public ButtonBinding KeyGraphics { get; set; } = new ButtonBinding();
+        [DefaultButtonBinding(0, Keys.M)] public ButtonBinding KeyCamera { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.M)]
-		public ButtonBinding KeyCamera { get; set; } = new ButtonBinding();
+        // Multiple keys are not supported, so we only set Keys.OemMinus
+        [DefaultButtonBinding(0, Keys.OemMinus)]
+        public ButtonBinding KeySaveState { get; set; } = new ButtonBinding();
 
-		// Multiple keys are not supported, so we only set Keys.OemMinus
-		[DefaultButtonBinding(0, Keys.OemMinus)]
-		public ButtonBinding KeySaveState { get; set; } = new ButtonBinding();
+        [DefaultButtonBinding(0, Keys.OemPlus)]
+        public ButtonBinding KeyLoadState { get; set; } = new ButtonBinding();
 
-		[DefaultButtonBinding(0, Keys.OemPlus)]
-		public ButtonBinding KeyLoadState { get; set; } = new ButtonBinding();
+        #region SimplifiedGraphics
 
-		#region SimplifiedGraphics
+        private bool _simplifiedGraphics = false;
 
-		private bool _simplifiedGraphics = false;
-		public bool SimplifiedGraphics {
-			get => Enabled && _simplifiedGraphics;
-			set => _simplifiedGraphics = value;
-		}
+        public bool SimplifiedGraphics {
+            get => Enabled && _simplifiedGraphics;
+            set => _simplifiedGraphics = value;
+        }
 
-		public int? SimplifiedLighting { get; set; } = 10;
-		public int? SimplifiedBloomBase { get; set; } = 0;
-		public int? SimplifiedBloomStrength { get; set; } = 1;
-		public SimplifiedGraphicsFeature.SpinnerColor SimplifiedSpinnerColor { get; set; } = SimplifiedGraphicsFeature.SpinnerColor.All[1];
-		public Color? SimplifiedDustSpriteColor { get; set; } = Color.Transparent;
-		public bool SimplifiedColorGrade { get; set; } = true;
-		public bool SimplifiedBackdrop { get; set; } = true;
-		public bool SimplifiedDecal { get; set; } = true;
-		public bool SimplifiedParticle { get; set; } = true;
-		public bool SimplifiedDistort { get; set; } = true;
-		public bool HideGameplay { get; set; } = false;
+        public int? SimplifiedLighting { get; set; } = 10;
+        public int? SimplifiedBloomBase { get; set; } = 0;
+        public int? SimplifiedBloomStrength { get; set; } = 1;
+        public SimplifiedGraphicsFeature.SpinnerColor SimplifiedSpinnerColor { get; set; } = SimplifiedGraphicsFeature.SpinnerColor.All[1];
+        public Color? SimplifiedDustSpriteColor { get; set; } = Color.Transparent;
+        public bool SimplifiedColorGrade { get; set; } = true;
+        public bool SimplifiedBackdrop { get; set; } = true;
+        public bool SimplifiedDecal { get; set; } = true;
+        public bool SimplifiedParticle { get; set; } = true;
+        public bool SimplifiedDistort { get; set; } = true;
 
-		#endregion
+        private bool _hideGamePlayer;
+        public bool HideGameplay {
+            get => _hideGamePlayer;
+            set => _hideGamePlayer = ShowHitboxes = value;
+        }
 
-		public bool ShowHitboxes {
-			get => GameplayRendererExt.RenderDebug;
-			set => GameplayRendererExt.RenderDebug = value;
-		}
+        #endregion
 
-		private bool _centerCamera = false;
-		public bool CenterCamera {
-			get => Enabled && _centerCamera;
-			set => _centerCamera = value;
-		}
+        public bool ShowHitboxes {
+            get => GameplayRendererExt.RenderDebug;
+            set => GameplayRendererExt.RenderDebug = value;
+        }
 
-		public bool DisableAchievements { get; set; } = false;
+        private bool _centerCamera = false;
 
-		[SettingNeedsRelaunch]
-		public bool UnixRTC { get; set; } = false;
+        public bool CenterCamera {
+            get => Enabled && _centerCamera;
+            set => _centerCamera = value;
+        }
 
-		[SettingNeedsRelaunch]
-		public bool LaunchStudioAtBoot { get; set; } = false;
+        public bool DisableAchievements { get; set; } = false;
 
-		[SettingIgnore]
-		public string DefaultPath { get; set; } = null;
+        [SettingNeedsRelaunch] public bool UnixRTC { get; set; } = false;
 
-		public bool Mod9DLighting { get; set; } = false;
+        [SettingNeedsRelaunch] public bool LaunchStudioAtBoot { get; set; } = false;
 
-		public bool DisableGrabDesyncFix {
-			get => Manager.grabButton != Buttons.Back;
-			set => Manager.grabButton = value ? Buttons.LeftShoulder : Buttons.Back;
-		}
+        [SettingIgnore] public string DefaultPath { get; set; } = null;
 
-		public bool RoundPosition { get; set; } = true;
+        public bool Mod9DLighting { get; set; } = false;
 
-		[SettingIgnore]
-		public bool FastForwardCallBase { get; set; } = false;
-		[SettingIgnore]
-		public int FastForwardThreshold { get; set; } = 10;
-		[SettingIgnore]
-		public DateTime StudioLastModifiedTime { get; set; } = new DateTime();
-		public bool AutoExtractNewStudio { get; set; } = true;
-		public bool AutoMute { get; set; } = true;
-		[SettingIgnore]
-		public Color EntityHitboxColor { get; set; } = HitboxColor.DefaultEntityColor;
-		[SettingIgnore]
-		public Color TriggerHitboxColor { get; set; } = HitboxColor.DefaultTriggerColor;
-		public bool HideTriggerHitboxes { get; set; } = false;
-		public bool SimplifiedHitboxes { get; set; } = true;
-		public ActualCollideHitboxTypes ShowActualCollideHitboxes { get; set; } = ActualCollideHitboxTypes.OFF;
-		public InfoPositions InfoHUD { get; set; } = InfoPositions.OFF;
-	}
+        public bool DisableGrabDesyncFix {
+            get => Manager.grabButton != Buttons.Back;
+            set => Manager.grabButton = value ? Buttons.LeftShoulder : Buttons.Back;
+        }
+
+        public bool RoundPosition { get; set; } = true;
+
+        [SettingIgnore] public bool FastForwardCallBase { get; set; } = false;
+        [SettingIgnore] public int FastForwardThreshold { get; set; } = 10;
+        [SettingIgnore] public DateTime StudioLastModifiedTime { get; set; } = new DateTime();
+        public bool AutoExtractNewStudio { get; set; } = true;
+        public bool AutoMute { get; set; } = true;
+        [SettingIgnore] public Color EntityHitboxColor { get; set; } = HitboxColor.DefaultEntityColor;
+        [SettingIgnore] public Color TriggerHitboxColor { get; set; } = HitboxColor.DefaultTriggerColor;
+        public bool HideTriggerHitboxes { get; set; } = false;
+        public bool SimplifiedHitboxes { get; set; } = true;
+        public ActualCollideHitboxTypes ShowActualCollideHitboxes { get; set; } = ActualCollideHitboxTypes.OFF;
+        public InfoPositions InfoHUD { get; set; } = InfoPositions.OFF;
+    }
 }
