b51d4bb6c9ff1945a15802c2b7aef720e10662ea
DemoJameson <demojameson@gmail.com>
2021-03-24 17:05:58 +0800

    feat: add "http://localhost:32270/tas/info" to show info

---
+++ CelesteTAS-EverestInterop/EverestInterop/DebugRcPage.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Celeste.Mod;
+
+namespace TAS.EverestInterop {
+    public static class DebugRcPage {
+        private static readonly RCEndPoint InfoEndPoint = new RCEndPoint {
+            Path = "/tas/info",
+            Name = "CelesteTAS Info",
+            InfoHTML = "List some tas info.",
+            Handle = c => {
+                StringBuilder builder = new StringBuilder();
+                Everest.DebugRC.WriteHTMLStart(c, builder);
+                WriteLine(builder, $"Running: {Manager.Running}");
+                WriteLine(builder, $"State: {Manager.State}");
+                WriteLine(builder, $"SaveState: {Savestates.StudioHighlightLine >= 0}");
+                WriteLine(builder, $"Player Info: ");
+                builder.Append($@"<pre>{PlayerInfo.Status}</pre>");
+                Everest.DebugRC.WriteHTMLEnd(c, builder);
+                Everest.DebugRC.Write(c, builder.ToString());
+            }
+        };
+
+        [Load]
+        private static void Load() {
+            Everest.DebugRC.EndPoints.Add(InfoEndPoint);
+        }
+
+        [Unload]
+        private static void Unload() {
+            Everest.DebugRC.EndPoints.Remove(InfoEndPoint);
+        }
+
+        private static void WriteLine(StringBuilder builder, string text) {
+            builder.Append($@"{text}<br />");
+        }
+    }
+}
