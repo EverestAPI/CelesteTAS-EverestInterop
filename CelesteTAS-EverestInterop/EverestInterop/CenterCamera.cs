bb0d8852ee99506b183626cb995f2f30b58a542e
DemoJameson <demojameson@gmail.com>
2021-04-21 20:44:15 +0800

    fix: dusts and lights are misaligned when center camera is enabled

--- CelesteTAS-EverestInterop/EverestInterop/CenterCamera.cs
+++ CelesteTAS-EverestInterop/EverestInterop/CenterCamera.cs
@@ -1,44 +1,63 @@
-﻿using Celeste;
+﻿using System;
+using Celeste;
 using Microsoft.Xna.Framework;
 using Monocle;
+using TAS.Utils;
 
 namespace TAS.EverestInterop {
     public static class CenterCamera {
-        private static Camera savedCamera;
-
-        public static CelesteTasModuleSettings Settings => CelesteTasModule.Settings;
+        private static Vector2? savedCameraPosition;
+        private static CelesteTasModuleSettings Settings => CelesteTasModule.Settings;
 
         public static void Load() {
-            // Optional: Center the camera
-            On.Celeste.Level.BeforeRender += Level_BeforeRender;
-            On.Celeste.Level.AfterRender += Level_AfterRender;
+            // note: change the camera.position before level.BeforeRender will cause desync 3A-roof04
+            On.Celeste.Level.Render += LevelOnRender;
+            On.Celeste.LightingRenderer.BeforeRender += LightingRendererOnRender;
+            On.Celeste.DustEdges.Render += DustEdgesOnRender;
         }
 
         public static void Unload() {
-            On.Celeste.Level.BeforeRender -= Level_BeforeRender;
-            On.Celeste.Level.AfterRender -= Level_AfterRender;
+            On.Celeste.Level.Render -= LevelOnRender;
+            On.Celeste.LightingRenderer.BeforeRender -= LightingRendererOnRender;
+            On.Celeste.DustEdges.Render -= DustEdgesOnRender;
         }
 
-        private static void Level_BeforeRender(On.Celeste.Level.orig_BeforeRender orig, Level self) {
-            orig.Invoke(self);
-            if (Settings.CenterCamera) {
-                Player player = self.Tracker.GetEntity<Player>();
-                if (player != null) {
-                    savedCamera = self.Camera;
-                    Vector2 cameraPosition = player.Position - new Vector2(savedCamera.Viewport.Width / 2, savedCamera.Viewport.Height / 2);
-                    self.Camera.Position = cameraPosition;
-                } else {
-                    savedCamera = null;
-                }
+        private static void CenterTheCamera(Action action) {
+            Camera camera = (Engine.Scene as Level)?.Camera;
+            if (Settings.CenterCamera && camera != null && Engine.Scene.GetPlayer() is { } player) {
+                savedCameraPosition = camera.Position;
+                camera.Position = player.Position - new Vector2(camera.Viewport.Width / 2f, camera.Viewport.Height / 2f);
+            }
+
+            action();
+
+            if (savedCameraPosition != null && camera != null) {
+                camera.Position = savedCameraPosition.Value;
+                savedCameraPosition = null;
             }
         }
 
-        private static void Level_AfterRender(On.Celeste.Level.orig_AfterRender orig, Level self) {
-            if (savedCamera != null) {
-                self.Camera.CopyFrom(savedCamera);
+        private static void LevelOnRender(On.Celeste.Level.orig_Render orig, Level self) {
+            CenterTheCamera(() => orig(self));
+        }
+
+        private static void LightingRendererOnRender(On.Celeste.LightingRenderer.orig_BeforeRender orig, LightingRenderer self, Scene scene) {
+            CenterTheCamera(() => orig(self, scene));
+        }
+
+        private static void DustEdgesOnRender(On.Celeste.DustEdges.orig_Render orig, DustEdges self) {
+            Vector2? origCameraPosition = null;
+            Camera camera = self.SceneAs<Level>()?.Camera;
+            if (Settings.CenterCamera && savedCameraPosition != null && camera != null) {
+                origCameraPosition = camera.Position;
+                camera.Position = savedCameraPosition.Value;
             }
 
-            orig.Invoke(self);
+            orig(self);
+
+            if (origCameraPosition != null) {
+                camera.Position = origCameraPosition.Value;
+            }
         }
     }
 }
