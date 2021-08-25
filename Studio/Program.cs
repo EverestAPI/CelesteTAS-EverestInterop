b4dd271921d13dd7713d6b74c98f8e336e057c3d
DemoJameson <demojameson@gmail.com>
2021-08-25 12:07:38 +0800

    fix(studio): studio does not restore form minimized when opening file

--- Studio/Program.cs
+++ Studio/Program.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 using Microsoft.VisualBasic.ApplicationServices;
@@ -48,10 +49,45 @@ namespace CelesteStudio {
 
         private void OnStartupNextInstance(object sender, StartupNextInstanceEventArgs e) {
             (MainForm as Studio)?.TryOpenFile(e.CommandLine.ToArray());
+            if (MainForm.WindowState == FormWindowState.Minimized) {
+                Restore(MainForm);
+            } else {
+                SetForegroundWindowEx(MainForm);
+            }
         }
 
         protected override void OnCreateMainForm() {
             MainForm = new Studio(CommandLineArgs.ToArray());
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hWnd, uint msg);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr processId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint GetCurrentThreadId();
+
+        private static void Restore(Form form) {
+            ShowWindow(form.Handle, 0x09);
+        }
+
+        private void SetForegroundWindowEx(Form form) {
+            uint appThread = GetCurrentThreadId();
+            uint foregroundThread = GetWindowThreadProcessId(GetForegroundWindow(), IntPtr.Zero);
+
+            if (foregroundThread != appThread) {
+                AttachThreadInput(foregroundThread, appThread, true);
+                form.Activate();
+                AttachThreadInput(foregroundThread, appThread, false);
+            }
         }
     }
 }
