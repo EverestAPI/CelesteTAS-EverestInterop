895fb43f4d74467e72856157ad827dc815c766b6
DemoJameson <demojameson@gmail.com>
2021-10-13 09:13:40 +0800

    fix: distortion effect is not disabled when showing hitboxes

--- CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxOptimized.cs
+++ CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxOptimized.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Celeste;
 using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
@@ -31,6 +32,8 @@ namespace TAS.EverestInterop.Hitboxes {
             On.Monocle.Circle.Render += CircleOnRender;
             On.Celeste.SoundSource.DebugRender += SoundSource_DebugRender;
             On.Celeste.Seeker.DebugRender += SeekerOnDebugRender;
+            IL.Celeste.Level.Render += Level_Render;
+            IL.Celeste.Pathfinder.Render += Pathfinder_Render;
         }
 
         [Unload]
@@ -43,6 +46,8 @@ namespace TAS.EverestInterop.Hitboxes {
             On.Monocle.Circle.Render -= CircleOnRender;
             On.Celeste.SoundSource.DebugRender -= SoundSource_DebugRender;
             On.Celeste.Seeker.DebugRender -= SeekerOnDebugRender;
+            IL.Celeste.Level.Render -= Level_Render;
+            IL.Celeste.Pathfinder.Render -= Pathfinder_Render;
         }
 
         private static void ModDebugRender(On.Monocle.Entity.orig_DebugRender orig, Entity self, Camera camera) {
@@ -208,6 +213,43 @@ namespace TAS.EverestInterop.Hitboxes {
                 pushRadius.Render(camera, HitboxColor.EntityColor);
                 self.Collider = origCollider;
             }
+        }
+
+        private static void Level_Render(ILContext il) {
+            ILCursor c;
+            new ILCursor(il).FindNext(out ILCursor[] found,
+                i => i.MatchLdfld(typeof(Pathfinder), "DebugRenderEnabled"),
+                i => i.MatchCall(typeof(Draw), "get_SpriteBatch"),
+                i => i.MatchLdarg(0),
+                i => i.MatchLdarg(0),
+                i => i.MatchLdarg(0)
+            );
+
+            // Place labels at and after pathfinder rendering code
+            ILLabel render = il.DefineLabel();
+            ILLabel skipRender = il.DefineLabel();
+            c = found[1];
+            c.MarkLabel(render);
+            c = found[4];
+            c.MarkLabel(skipRender);
+
+            // || the value of DebugRenderEnabled with Debug rendering being enabled, && with seekers being present.
+            c = found[0];
+            c.Index++;
+            c.Emit(OpCodes.Brtrue_S, render.Target);
+            c.EmitDelegate<Func<bool>>(() => Settings.ShowHitboxes);
+            c.Emit(OpCodes.Brfalse_S, skipRender.Target);
+            c.Emit(OpCodes.Ldarg_0);
+            c.Emit(OpCodes.Callvirt, typeof(Scene).GetMethod("get_Tracker"));
+            MethodInfo getEntity = typeof(Tracker).GetMethod("GetEntity");
+            c.Emit(OpCodes.Callvirt, getEntity.MakeGenericMethod(new Type[] {typeof(Seeker)}));
+        }
+
+        private static void Pathfinder_Render(ILContext il) {
+            // Remove the for loop which draws pathfinder tiles
+            ILCursor c = new(il);
+            c.FindNext(out ILCursor[] found, i => i.MatchLdfld(typeof(Pathfinder), "lastPath"));
+            c.RemoveRange(found[0].Index - 1);
         }
     }
 }
--- CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxToggle.cs
+++ CelesteTAS-EverestInterop/EverestInterop/Hitboxes/HitboxToggle.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using Celeste;
+using Microsoft.Xna.Framework.Graphics;
 using MonoMod.Cil;
 
 namespace TAS.EverestInterop.Hitboxes {
     public static class HitboxToggle {
+        private static CelesteTasModuleSettings Settings => CelesteTasModule.Settings;
+
         [Load]
         private static void Load() {
             IL.Celeste.GameplayRenderer.Render += GameplayRendererOnRender;
+            On.Celeste.Distort.Render += Distort_Render;
         }
 
         [Unload]
         private static void Unload() {
             IL.Celeste.GameplayRenderer.Render -= GameplayRendererOnRender;
+            On.Celeste.Distort.Render -= Distort_Render;
         }
 
         private static void GameplayRendererOnRender(ILContext il) {
@@ -20,6 +25,16 @@ namespace TAS.EverestInterop.Hitboxes {
                 ilCursor.EmitDelegate<Func<bool, bool>>(renderDebug =>
                     renderDebug || CelesteTasModule.Settings.ShowHitboxes || !CelesteTasModule.Settings.ShowGameplay);
             }
+        }
+
+        private static void Distort_Render(On.Celeste.Distort.orig_Render orig, Texture2D source, Texture2D map, bool hasDistortion) {
+            if (Settings.ShowHitboxes || Settings.SimplifiedGraphics && Settings.SimplifiedDistort) {
+                Distort.Anxiety = 0f;
+                Distort.GameRate = 1f;
+                hasDistortion = false;
+            }
+
+            orig(source, map, hasDistortion);
         }
     }
 }
