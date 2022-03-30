9ae679577fda4fe7b660864df793aac2a00544b1
DemoJameson <demojameson@gmail.com>
2022-03-30 23:15:12 +0800

    fix(studio): studio can't launch with invalid toml file

--- Studio/Settings.cs
+++ Studio/Settings.cs
@@ -7,44 +7,53 @@ namespace CelesteStudio;
 
 [TommyTableName("Settings")]
 public class Settings {
-    private const string path = @".\Celeste Studio.toml";
-    public static Settings Instance { get; private set; }
+    private const string path = "Celeste Studio.toml";
+    public static Settings Instance { get; private set; } = new();
 
-    public int LocationX = 100;
-    public int LocationY = 100;
+    [TommyInclude] private int locationX = 100;
+    [TommyInclude] private int locationY = 100;
 
     [TommyIgnore]
     public Point Location {
-        get => new(LocationX, LocationY);
+        get => new(locationX, locationY);
         set {
-            LocationX = value.X;
-            LocationY = value.Y;
+            locationX = value.X;
+            locationY = value.Y;
         }
     }
 
-    public int Width = 400;
-    public int Height = 800;
+    [TommyInclude] private int width = 400;
+    [TommyInclude] private int height = 800;
 
     [TommyIgnore]
     public Size Size {
-        get => new(Width, Height);
+        get => new(width, height);
         set {
-            Width = value.Width;
-            Height = value.Height;
+            width = value.Width;
+            height = value.Height;
         }
     }
 
-    public string FontName = "Courier New";
-    public float FontSize = 14.25f;
-    public byte FontStyle;
+    [TommyInclude] private string fontName = "Courier New";
+    [TommyInclude] private float fontSize = 14.25f;
+    [TommyInclude] private byte fontStyle;
 
     [TommyIgnore]
     public Font Font {
-        get => new(new FontFamily(FontName), FontSize, (FontStyle) FontStyle);
+        get {
+            try {
+                return new Font(new FontFamily(fontName), fontSize, (FontStyle) fontStyle);
+            } catch {
+                fontName = "Courier New";
+                fontSize = 14.25f;
+                fontStyle = 0;
+                return new Font(new FontFamily(fontName), fontSize, (FontStyle) fontStyle);
+            }
+        }
         set {
-            FontName = value.FontFamily.Name;
-            FontSize = value.Size;
-            FontStyle = (byte) value.Style;
+            fontName = value.FontFamily.Name;
+            fontSize = value.Size;
+            fontStyle = (byte) value.Style;
         }
     }
 
@@ -55,15 +64,27 @@ public class Settings {
     public int AutoBackupRate = 1;
     public int AutoBackupCount = 100;
     public bool FindMatchCase;
-    public string LastFileName;
+
+    public string LastFileName = "";
+
+    // ReSharper disable once FieldCanBeMadeReadOnly.Global
     public List<string> RecentFiles = new();
 
     public static void Load() {
-        Instance = File.Exists(path) ? TommySerializer.FromTomlFile<Settings>(path) : new Settings();
+        try {
+            if (File.Exists(path)) {
+                Instance = TommySerializer.FromTomlFile<Settings>(path);
+            }
+        } catch {
+            // ignore
+        }
     }
 
     public static void Save() {
-        Instance ??= new Settings();
-        TommySerializer.ToTomlFile(Instance, path);
+        try {
+            TommySerializer.ToTomlFile(Instance, path);
+        } catch {
+            // ignore
+        }
     }
 }
