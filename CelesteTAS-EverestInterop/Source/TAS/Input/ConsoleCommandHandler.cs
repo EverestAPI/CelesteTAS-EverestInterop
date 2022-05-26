9b934f4c882f52180a463eaddb0dfd8eae22a7b8
DemoJameson <demojameson@gmail.com>
2022-05-26 21:27:14 +0800

    feat: make the hearts command for mini heart door without specifying levelset

--- CelesteTAS-EverestInterop/Source/TAS/Input/ConsoleCommandHandler.cs
+++ CelesteTAS-EverestInterop/Source/TAS/Input/ConsoleCommandHandler.cs
@@ -340,4 +340,39 @@ public static class ConsoleCommandHandler {
         stats.UnlockedAreas = 1;
         stats.AreasIncludingCeleste[0].Modes[0].Completed = true;
     }
+
+    [Monocle.Command("hearts",
+        "sets the amount of obtained hearts for the specified level set to a given number (default all hearts and current level set) (support mini heart door via CelesteTAS)")]
+    private static void CmdHearts(int amount = int.MaxValue, string levelSet = null) {
+        SaveData saveData = SaveData.Instance;
+        if (saveData == null) {
+            return;
+        }
+
+        if (string.IsNullOrEmpty(levelSet)) {
+            const string miniHeartDoorFullName = "Celeste.Mod.CollabUtils2.Entities.MiniHeartDoor";
+
+            if (Engine.Scene.Entities.FirstOrDefault(e => e.GetType().FullName == miniHeartDoorFullName) is { } miniHeartDoor) {
+                levelSet = miniHeartDoor.GetFieldValue<string>("levelSet");
+            } else {
+                levelSet = saveData.GetLevelSet();
+            }
+        }
+
+        int num = 0;
+        foreach (AreaStats areaStats in saveData.Areas_Safe.Where(stats => stats.LevelSet == levelSet)) {
+            for (int i = 0; i < areaStats.Modes.Length; i++) {
+                if (AreaData.Get(areaStats.ID).Mode is not { } mode || mode.Length <= i || mode[i]?.MapData == null)
+                    continue;
+
+                AreaModeStats areaModeStats = areaStats.Modes[i];
+                if (num < amount) {
+                    areaModeStats.HeartGem = true;
+                    num++;
+                } else {
+                    areaModeStats.HeartGem = false;
+                }
+            }
+        }
+    }
 }
