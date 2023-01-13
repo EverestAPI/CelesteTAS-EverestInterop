1550e16a96b848ba54c137dfc05cab16c537c178
DemoJameson <demojameson@gmail.com>
2023-01-13 10:26:02 +0800

    feat: disable controller rumble during TAS playback

---
+++ CelesteTAS-EverestInterop/Source/EverestInterop/DisableRumble.cs
@@ -0,0 +1,16 @@
+﻿using Monocle;
+using TAS.Module;
+using TAS.Utils;
+
+namespace TAS.EverestInterop;
+
+public static class DisableRumble {
+    [Load]
+    private static void Load() {
+        HookHelper.SkipMethod(typeof(DisableRumble), nameof(IsDisableRumble), typeof(MInput.GamePadData).GetMethodInfo("Rumble"));
+    }
+
+    private static bool IsDisableRumble() {
+        return Manager.Running;
+    }
+}
