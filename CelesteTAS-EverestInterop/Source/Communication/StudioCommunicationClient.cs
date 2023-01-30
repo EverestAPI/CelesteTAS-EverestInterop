d5b073a149cfe584e6c328b6f81cfec1bbd1f11b
DemoJameson <demojameson@gmail.com>
2023-01-30 17:38:56 +0800

    build: bump mod v3.21.0, studio v2.11.0

--- CelesteTAS-EverestInterop/Source/Communication/StudioCommunicationClient.cs
+++ CelesteTAS-EverestInterop/Source/Communication/StudioCommunicationClient.cs
@@ -432,7 +432,8 @@ public sealed class StudioCommunicationClient : StudioCommunicationBase {
     }
 
     private void SendModVersion() {
-        const string minStudioVersion = "2.10.2";
+        // TODO: move to everest.yaml
+        const string minStudioVersion = "2.11.0";
         byte[] data = BinaryFormatterHelper.ToByteArray(new[] {CelesteTasModule.Instance.Metadata.VersionString, minStudioVersion});
         WriteMessageGuaranteed(new Message(MessageID.VersionInfo, data));
     }
