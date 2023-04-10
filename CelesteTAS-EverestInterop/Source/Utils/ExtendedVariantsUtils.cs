1af5b04d0408ac2dc2992ad0d71de667a08cd4a8
DemoJameson <demojameson@gmail.com>
2023-04-10 18:01:36 +0800

    fix: game crashes when ExtendedVariantMode is not installed

--- CelesteTAS-EverestInterop/Source/Utils/ExtendedVariantsUtils.cs
+++ CelesteTAS-EverestInterop/Source/Utils/ExtendedVariantsUtils.cs
@@ -7,19 +7,19 @@ internal static class ExtendedVariantsUtils {
     private static readonly Lazy<bool> installed = new(() => ModUtils.GetModule("ExtendedVariantMode") != null);
 
     // enum value might be different between different ExtendedVariantMode version
-    private static readonly Lazy<ExtendedVariantsModule.Variant> upsideDownVariant =
-        new(() => (ExtendedVariantsModule.Variant) Enum.Parse(typeof(ExtendedVariantsModule.Variant), "UpsideDown"));
+    private static readonly Lazy<object> upsideDownVariant =
+        new(() => Enum.Parse(typeof(ExtendedVariantsModule.Variant), "UpsideDown"));
 
-    private static readonly Lazy<ExtendedVariantsModule.Variant> superDashingVariant =
-        new(() => (ExtendedVariantsModule.Variant) Enum.Parse(typeof(ExtendedVariantsModule.Variant), "SuperDashing"));
+    private static readonly Lazy<object> superDashingVariant =
+        new(() => Enum.Parse(typeof(ExtendedVariantsModule.Variant), "SuperDashing"));
 
     private static bool upsideDown =>
-        (bool) ExtendedVariantsModule.Instance.TriggerManager.GetCurrentVariantValue(upsideDownVariant.Value);
+        (bool) ExtendedVariantsModule.Instance.TriggerManager.GetCurrentVariantValue((ExtendedVariantsModule.Variant) upsideDownVariant.Value);
 
     public static bool UpsideDown => installed.Value && upsideDown;
 
     private static bool superDashing =>
-        (bool) ExtendedVariantsModule.Instance.TriggerManager.GetCurrentVariantValue(superDashingVariant.Value);
+        (bool) ExtendedVariantsModule.Instance.TriggerManager.GetCurrentVariantValue((ExtendedVariantsModule.Variant) superDashingVariant.Value);
 
     public static bool SuperDashing => installed.Value && superDashing;
 }
