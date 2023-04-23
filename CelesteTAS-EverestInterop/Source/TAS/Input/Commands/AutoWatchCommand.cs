924a4f5fa2c00ac874c1f322711d037f0cb33057
XMinty7 <122070755+XMinty7@users.noreply.github.com>
2023-04-23 20:34:51 +0100

    feat: improve AutoWatch command
    - support for multiple types at once
    - distinct EndAutoWatch command for unwatching
    - fixed documentation errors for the command

--- CelesteTAS-EverestInterop/Source/TAS/Input/Commands/AutoWatchCommand.cs
+++ CelesteTAS-EverestInterop/Source/TAS/Input/Commands/AutoWatchCommand.cs
@@ -15,45 +15,70 @@ public static class AutoWatchCommand {
     private const string logPrefix = "{AutoWatch Command Failed: }";
     private static readonly object nonReturnObject = new();
 
-    [Monocle.Command("autowatch", "Add Type to auto watch list, with optional onscreen limiter - e.g. AutoWatch Kevin, AutoWatch Bumper OnScreen")]
-    private static void Invoke(string arg1, string arg2, string arg3, string arg4, string arg5, string arg6, string arg7, string arg8,
+    [Monocle.Command("autowatch", "Adds an entity type(s) to auto watch list e.g. autowatch Kevin, autowatch Bumper")]
+    private static void InvokeWatch(string arg1, string arg2, string arg3, string arg4, string arg5, string arg6, string arg7, string arg8,
         string arg9) {
         string[] args = {arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9};
         consolePrintLog = true;
-        Invoke(args.TakeWhile(arg => arg != null).ToArray());
+        InvokeWatch(args.TakeWhile(arg => arg != null).ToArray());
         consolePrintLog = false;
     }
 
-    // AutoWatch, Type, [OnScreen]
+    // AutoWatch, Type1, Type2, ...
     [TasCommand("AutoWatch")]
-    private static void Invoke(string[] args) {
-        if (args.Length == 0) {
-            return;
-        }
+    private static void InvokeWatch(string[] args) {
+        List<Type> watchTypes = new();
 
-        Type watchType = null;
-        bool unwatch = false;
-
-        try {
-            if (!InfoCustom.TryParseType(args[0], out watchType, out _, out string errorMessage)) {
-                errorMessage.Log(consolePrintLog, LogLevel.Warn);
-            }
-        } catch (Exception e) {
-            e.Log(consolePrintLog, LogLevel.Warn);
-        }
-
-        if (watchType is null) return;
-
-        for (int i = 1; i < args.Length; i++) {
-            if (args[i].ToLowerInvariant() == "unwatch") {
-                unwatch = true;
+        foreach (string arg in args) {
+            try {
+                if (!InfoCustom.TryParseType(arg, out Type watchType, out _, out string errorMessage)) {
+                    errorMessage.Log(consolePrintLog, LogLevel.Warn);
+                } else {
+                    watchTypes.Add(watchType);
+                }
+            } catch (Exception e) {
+                e.Log(consolePrintLog, LogLevel.Warn);
             }
         }
 
-        if (unwatch) {
-            InfoWatchEntity.AutoWatch.Remove(watchType);
-        } else {
+        foreach (var watchType in watchTypes) {
             InfoWatchEntity.AutoWatch.Add(watchType);
+        }
+    }
+
+    [Monocle.Command("endautowatch", "Removes an entity type(s) from auto watch list e.g. endautowatch Kevin. Don't specify entity types to stop all autowatching")]
+    private static void InvokeEndWatch(string arg1, string arg2, string arg3, string arg4, string arg5, string arg6, string arg7, string arg8,
+        string arg9) {
+        string[] args = { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 };
+        consolePrintLog = true;
+        InvokeEndWatch(args.TakeWhile(arg => arg != null).ToArray());
+        consolePrintLog = false;
+    }
+
+    // EndAutoWatch, Type1, Type2, ...
+    // EndAutoWatch
+    [TasCommand("EndAutoWatch")]
+    private static void InvokeEndWatch(string[] args) {
+        List<Type> watchTypes = new();
+
+        foreach (string arg in args) {
+            try {
+                if (!InfoCustom.TryParseType(arg, out Type watchType, out _, out string errorMessage)) {
+                    errorMessage.Log(consolePrintLog, LogLevel.Warn);
+                } else {
+                    watchTypes.Add(watchType);
+                }
+            } catch (Exception e) {
+                e.Log(consolePrintLog, LogLevel.Warn);
+            }
+        }
+
+        if (watchTypes.Count == 0) {
+            InfoWatchEntity.AutoWatch.Clear();
+        }
+
+        foreach (var watchType in watchTypes) {
+            InfoWatchEntity.AutoWatch.Remove(watchType);
         }
     }
 }
