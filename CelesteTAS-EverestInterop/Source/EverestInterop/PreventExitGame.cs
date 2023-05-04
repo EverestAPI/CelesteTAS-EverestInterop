262514601fe3029b774d3c72f1b4a9f814c3f3ca
DemoJameson <demojameson@gmail.com>
2023-05-04 15:18:45 +0800

    feat: prevent exiting the game by the main menu when tas is running

---
+++ CelesteTAS-EverestInterop/Source/EverestInterop/PreventExitGame.cs
@@ -0,0 +1,24 @@
+﻿using TAS.Module;
+
+namespace TAS.EverestInterop;
+
+public static class PreventExitGame {
+    [Load]
+    private static void Load() {
+        On.Celeste.OuiMainMenu.OnExit += OuiMainMenuOnOnExit;
+    }
+
+    [Unload]
+    private static void Unload() {
+        On.Celeste.OuiMainMenu.OnExit -= OuiMainMenuOnOnExit;
+    }
+
+    private static void OuiMainMenuOnOnExit(On.Celeste.OuiMainMenu.orig_OnExit orig, Celeste.OuiMainMenu self) {
+        if (Manager.Running) {
+            AbortTas("To close the game while TAS is running\nuse the ExitGame command instead");
+            return;
+        }
+
+        orig(self);
+    }
+}
