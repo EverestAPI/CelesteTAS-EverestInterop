922a8c0119dcfcea3491593d38b6c6b7676085dc
DemoJameson <demojameson@gmail.com>
2023-05-13 13:14:09 +0800

    refactor: replace a few hooks with ilhook

--- CelesteTAS-EverestInterop/Source/EverestInterop/DesyncFixer.cs
+++ CelesteTAS-EverestInterop/Source/EverestInterop/DesyncFixer.cs
@@ -53,63 +53,47 @@ public static class DesyncFixer {
 
     [Load]
     private static void Load() {
-        On.Celeste.DreamMirror.ctor += DreamMirrorOnCtor;
-        On.Celeste.FinalBoss.ctor_Vector2_Vector2Array_int_float_bool_bool_bool += FinalBossOnCtor_Vector2_Vector2Array_int_float_bool_bool_bool;
-        On.Celeste.CS03_Memo.MemoPage.ctor += MemoPageOnCtor;
-        On.Monocle.Entity.Update += EntityOnUpdate;
+        typeof(DreamMirror).GetMethod("Added").HookAfter<DreamMirror>(FixDreamMirrorDesync);
+        typeof(CS03_Memo.MemoPage).GetConstructors()[0].HookAfter<CS03_Memo.MemoPage>(FixMemoPageCrash);
+        typeof(FinalBoss).GetMethod("Added").HookAfter<FinalBoss>(FixFinalBossDesync);
+        typeof(Entity).GetMethod("Update").HookAfter(AfterEntityUpdate);
     }
 
-    [Unload]
-    private static void Unload() {
-        On.Celeste.DreamMirror.ctor -= DreamMirrorOnCtor;
-        On.Celeste.FinalBoss.ctor_Vector2_Vector2Array_int_float_bool_bool_bool -= FinalBossOnCtor_Vector2_Vector2Array_int_float_bool_bool_bool;
-        On.Celeste.CS03_Memo.MemoPage.ctor -= MemoPageOnCtor;
-        On.Monocle.Entity.Update -= EntityOnUpdate;
-    }
-
-    private static void DreamMirrorOnCtor(On.Celeste.DreamMirror.orig_ctor orig, DreamMirror self, Vector2 position) {
-        orig(self, position);
-
-        self.Add(new PostUpdateHook(() => {
+    private static void FixDreamMirrorDesync(DreamMirror mirror) {
+        mirror.Add(new PostUpdateHook(() => {
             if (Manager.Running) {
                 // DreamMirror does some dirty stuff in BeforeRender.
-                self.BeforeRender();
+                mirror.BeforeRender();
             }
         }));
     }
 
-    private static void FinalBossOnCtor_Vector2_Vector2Array_int_float_bool_bool_bool(
-        On.Celeste.FinalBoss.orig_ctor_Vector2_Vector2Array_int_float_bool_bool_bool orig, FinalBoss self, Vector2 position, Vector2[] nodes,
-        int patternIndex, float cameraYPastMax, bool dialog, bool startHit, bool cameraLockY) {
-        orig(self, position, nodes, patternIndex, cameraYPastMax, dialog, startHit, cameraLockY);
-
-        self.Add(new PostUpdateHook(() => {
+    private static void FixFinalBossDesync(FinalBoss finalBoss) {
+        finalBoss.Add(new PostUpdateHook(() => {
             if (!Manager.Running) {
                 return;
             }
 
             // FinalBoss does some dirty stuff in Render.
             // finalBoss.ShotOrigin => base.Center + Sprite.Position + new Vector2(6f * Sprite.Scale.X, 2f);
-            if (self.Sprite is { } sprite) {
-                sprite.Scale.X = self.facing;
+            if (finalBoss.Sprite is { } sprite) {
+                sprite.Scale.X = finalBoss.facing;
                 sprite.Scale.Y = 1f;
-                sprite.Scale *= 1f + self.scaleWiggler.Value * 0.2f;
+                sprite.Scale *= 1f + finalBoss.scaleWiggler.Value * 0.2f;
             }
         }));
     }
 
-    private static void MemoPageOnCtor(On.Celeste.CS03_Memo.MemoPage.orig_ctor orig, Entity self) {
-        orig(self);
-        self.Add(new PostUpdateHook(() => {
-            if (Manager.Running && self is CS03_Memo.MemoPage {target: null} memoPage) {
+    private static void FixMemoPageCrash(CS03_Memo.MemoPage memoPage) {
+        memoPage.Add(new PostUpdateHook(() => {
+            if (Manager.Running && memoPage.target == null) {
                 // initialize memoPage.target, fix game crash when fast forward
                 memoPage.BeforeRender();
             }
         }));
     }
 
-    private static void EntityOnUpdate(On.Monocle.Entity.orig_Update orig, Entity self) {
-        orig(self);
+    private static void AfterEntityUpdate() {
         debrisAmount = 0;
     }
 
