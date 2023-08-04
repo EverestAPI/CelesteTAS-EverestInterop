d6d427789be17ca33b532201413e8aa0c486a97e
DemoJameson <demojameson@gmail.com>
2023-08-04 15:07:25 +0800

    feat: stop tas when critical error handler is showing with latest core everest

--- CelesteTAS-EverestInterop/Source/EverestInterop/CriticalErrorHandlerFixer.cs
+++ CelesteTAS-EverestInterop/Source/EverestInterop/CriticalErrorHandlerFixer.cs
@@ -1,5 +1,5 @@
 using System;
-using Celeste;
+using MonoMod.Utils;
 using TAS.Module;
 using TAS.Utils;
 
@@ -7,17 +7,19 @@ namespace TAS.EverestInterop;
 
 public static class CriticalErrorHandlerFixer {
     public static bool Handling => getCurrentHandler?.Invoke(null) != null;
-    private static GetDelegate<Overlay, Overlay> getCurrentHandler;
+    private static FastReflectionDelegate getCurrentHandler;
 
     [Load]
     private static void Load() {
         Type type = ModUtils.VanillaAssembly.GetType("Celeste.Mod.UI.CriticalErrorHandler");
-        type?.GetMethod("Update")?.HookBefore(() => {
-            if (Manager.Running) {
-                Manager.DisableRun();
-            }
-        });
+        if (type != null) {
+            type.GetMethod("Update")?.HookBefore(() => {
+                if (Manager.Running) {
+                    Manager.DisableRun();
+                }
+            });
 
-        getCurrentHandler = type?.CreateGetDelegate<Overlay, Overlay>("CurrentHandler");
+            getCurrentHandler = type.GetProperty("CurrentHandler")?.GetGetMethod()?.GetFastDelegate();
+        }
     }
 }
