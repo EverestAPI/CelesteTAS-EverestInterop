b4ee427cf4007cc338e9b06648dc1ac13ed24af5
psyGamer <psygamer21@gmail.com>
2024-06-21 16:31:12 +0200

    Setup CelesteStudio2 project

---
+++ CelesteStudio2/CelesteStudio2.Gtk/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Eto.Forms;
+
+namespace CelesteStudio2.Gtk {
+    class Program {
+        [STAThread]
+        public static void Main(string[] args) {
+            new Application(Eto.Platforms.Gtk).Run(new MainForm());
+        }
+    }
+}
---
+++ CelesteStudio2/CelesteStudio2.Wpf/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Eto.Forms;
+
+namespace CelesteStudio2.Wpf {
+    class Program {
+        [STAThread]
+        public static void Main(string[] args) {
+            new Application(Eto.Platforms.Wpf).Run(new MainForm());
+        }
+    }
+}
