024843c011229efb7a2f70905946a6501ca87dcb
psyGamer <psygamer21@gmail.com>
2024-07-12 20:57:39 +0200

    Remove "CelesteStudio.Controls" import

--- Studio/CelesteStudio/Editing/ThemeEditor.cs
+++ Studio/CelesteStudio/Editing/ThemeEditor.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using CelesteStudio.Controls;
 using CelesteStudio.Editing;
 
 namespace CelesteStudio;
