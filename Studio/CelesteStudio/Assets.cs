8eff5824666296effced5e5fdd65dc42299671bd
psyGamer <psygamer21@gmail.com>
2024-07-17 13:24:06 +0200

    Don't use negative widths for popout icon path

--- Studio/CelesteStudio/Assets.cs
+++ Studio/CelesteStudio/Assets.cs
@@ -22,11 +22,11 @@ public static class Assets {
         path.AddLines([new(0.6f, 0.1f), new(0.9f, 0.1f), new(0.9f, 0.4f), new(0.8f, 0.4f), new(0.8f, 0.25625f), new(0.535417f, 0.53541f), new(0.464584f, 0.464584f), new(0.72917f, 0.2f), new(0.6f, 0.2f)]);
         path.StartFigure();
         path.AddLines([new(0.8f, 0.8f), new(0.8f, 0.5f), new(0.7f, 0.5f), new(0.7f, 0.8f), new(0.2f, 0.8f), new(0.2f, 0.3f), new(0.5f, 0.3f), new(0.5f, 0.2f), new(0.2f, 0.2f)]);
-        path.AddArc(path.CurrentPoint.X, path.CurrentPoint.Y, -0.1f, 0.1f, 270.0f, 90.0f);
+        path.AddArc(path.CurrentPoint.X - 0.1f, path.CurrentPoint.Y, 0.1f, 0.1f, 270.0f, -90.0f);
         path.AddLine(path.CurrentPoint.X, path.CurrentPoint.Y, 0.1f, 0.8f);
         path.AddArc(path.CurrentPoint.X, path.CurrentPoint.Y, 0.1f, 0.1f, 180.0f, -90.0f);
         path.AddLine(path.CurrentPoint.X, path.CurrentPoint.Y, 0.7f, 0.9f);
-        path.AddArc(path.CurrentPoint.X, path.CurrentPoint.Y, 0.1f, -0.1f, 270.0f, 90.0f);
+        path.AddArc(path.CurrentPoint.X, path.CurrentPoint.Y - 0.1f, 0.1f, 0.1f, 90.0f, -90.0f);
             
         return path;
     }
