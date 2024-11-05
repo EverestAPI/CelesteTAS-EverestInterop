8a95136fe89b7779d8fd9f66f4b4f75caedc8a9b
psyGamer <psygamer21@gmail.com>
2024-11-05 22:29:56 +0100

    fix(Studio): A few issues with the macOS SkiaSharp rendering

--- Studio/CelesteStudio.Mac/SkiaDrawableHandler.cs
+++ Studio/CelesteStudio.Mac/SkiaDrawableHandler.cs
@@ -2,9 +2,8 @@ using CelesteStudio.Controls;
 using Eto.Mac.Forms;
 using MonoMac.AppKit;
 using MonoMac.CoreGraphics;
+using MonoMac.Foundation;
 using SkiaSharp;
-using System;
-using System.Runtime.InteropServices;
 
 namespace CelesteStudio.Mac;
 
@@ -21,45 +20,29 @@ public class SkiaDrawableHandler : MacPanel<SkiaDrawableHandler.SkiaDrawableView
     }
 
     public class SkiaDrawableView(SkiaDrawable drawable) : MacPanelView {
-        private IntPtr bitmapData;
-        private int lastLength;
-
+        private NSMutableData? bitmapData;
+        private CGDataProvider? dataProvider;
         private SKSurface? surface;
 
         private readonly CGColorSpace colorSpace = CGColorSpace.CreateDeviceRGB();
-        private CGDataProvider? dataProvider;
-        private CGImage? image;
 
-        public override void DrawRect(CGRect dirtyRect)
-        {
+        public override void DrawRect(CGRect dirtyRect) {
             base.DrawRect(dirtyRect);
 
-            var bounds = new CGRect(drawable.DrawX, drawable.DrawY, drawable.DrawWidth, drawable.DrawHeight);
+            var bounds = new CGRect(drawable.DrawX, drawable.DrawY, drawable.DrawWidth * Window.BackingScaleFactor, drawable.DrawHeight * Window.BackingScaleFactor);
             var info = new SKImageInfo((int)bounds.Width, (int)bounds.Height, SKColorType.Rgba8888, SKAlphaType.Premul);
 
-            // Allocate a memory block for the drawing process
-            int newLength = info.BytesSize;
-            if (lastLength != newLength)
-            {
-                lastLength = newLength;
-                if (bitmapData != IntPtr.Zero) {
-                    bitmapData = Marshal.ReAllocCoTaskMem(bitmapData, newLength);
-                } else {
-                    bitmapData = Marshal.AllocCoTaskMem(newLength);
-                }
+            // Allocate a memory for the drawing process
+            nuint infoLength = (nuint)info.BytesSize;
+            if (bitmapData?.Length != infoLength) {
+                dataProvider?.Dispose();
+                bitmapData?.Dispose();
+
+                bitmapData = NSMutableData.FromLength(infoLength);
+                dataProvider = new CGDataProvider(bitmapData.MutableBytes, info.BytesSize);
 
                 surface?.Dispose();
-                surface = SKSurface.Create(info, bitmapData, info.RowBytes);
-
-                dataProvider?.Dispose();
-                dataProvider = new CGDataProvider(bitmapData, lastLength);
-
-                image?.Dispose();
-                image = new CGImage(
-                    info.Width, info.Height,
-                    8, info.BitsPerPixel, info.RowBytes,
-                    colorSpace, CGBitmapFlags.ByteOrder32Big | CGBitmapFlags.PremultipliedLast,
-                    dataProvider, null, false, CGColorRenderingIntent.Default);
+                surface = SKSurface.Create(info, bitmapData.MutableBytes, info.RowBytes);
             }
 
             using (new SKAutoCanvasRestore(surface!.Canvas, true)) {
@@ -67,26 +50,27 @@ public class SkiaDrawableHandler : MacPanel<SkiaDrawableHandler.SkiaDrawableView
             }
             surface.Canvas.Flush();
 
+            using var image = new CGImage(
+                info.Width, info.Height,
+                8, info.BitsPerPixel, info.RowBytes,
+                colorSpace, CGBitmapFlags.ByteOrder32Big | CGBitmapFlags.PremultipliedLast,
+                dataProvider, null, false, CGColorRenderingIntent.Default);
+
             var ctx = NSGraphicsContext.CurrentContext.GraphicsPort;
             ctx.DrawImage(bounds, image);
         }
 
         protected override void Dispose(bool disposing) {
-            Marshal.FreeCoTaskMem(bitmapData);
+            dataProvider?.Dispose();
+            dataProvider = null;
 
-            bitmapData = IntPtr.Zero;
-            lastLength = 0;
+            bitmapData?.Dispose();
+            bitmapData = null;
 
             surface?.Dispose();
             surface = null;
 
             colorSpace.Dispose();
-
-            dataProvider?.Dispose();
-            dataProvider = null;
-
-            image?.Dispose();
-            image = null;
         }
     }
 }
