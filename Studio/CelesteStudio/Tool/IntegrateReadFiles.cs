f39d129d03b32d9828ba034f3804df01a23c8c39
psyGamer <psygamer21@gmail.com>
2025-01-24 13:00:11 +0100

    fix(Studio): Compilation error

--- Studio/CelesteStudio/Tool/IntegrateReadFiles.cs
+++ Studio/CelesteStudio/Tool/IntegrateReadFiles.cs
@@ -2,6 +2,7 @@ using CelesteStudio.Editing;
 using StudioCommunication;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace CelesteStudio.Tool;
