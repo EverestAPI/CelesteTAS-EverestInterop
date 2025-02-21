0d519951f2b5728b1f57f89d5cbdf3e3019398aa
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-02-21 11:46:57 +0100

    hotkey dialog: listen to TextInput as well
    While '^' gets a KeyCode char event, ö and ä are only accessible
    via text input.

--- Studio/CelesteStudio/Dialog/HotkeyDialog.cs
+++ Studio/CelesteStudio/Dialog/HotkeyDialog.cs
@@ -12,8 +12,14 @@ using Eto.Forms;
 namespace CelesteStudio.Dialog;
 
 public class HotkeyDialog : Dialog<Hotkey> {
+    private Dictionary<MenuEntry, Hotkey> keyBindings;
+    private List<Snippet> snippets;
+    private TextControl pressLabel;
+    
     private HotkeyDialog(Hotkey currentHotkey, Dictionary<MenuEntry, Hotkey> keyBindings, List<Snippet> snippets) {
-        var pressLabel = new Label { Text = "Press any key...", Font = SystemFonts.Bold().WithFontStyle(FontStyle.Bold | FontStyle.Italic) };
+        this.keyBindings = keyBindings;
+        this.snippets = snippets;
+        pressLabel = new Label { Text = "Press any key...", Font = SystemFonts.Bold().WithFontStyle(FontStyle.Bold | FontStyle.Italic) };
 
         Title = "Edit Hotkey";
         Content = new StackLayout {
@@ -55,56 +61,67 @@ public class HotkeyDialog : Dialog<Hotkey> {
             if (e.Key is Keys.LeftShift or Keys.RightShift
                 or Keys.LeftControl or Keys.RightControl
                 or Keys.LeftAlt or Keys.RightAlt
-                or Keys.LeftApplication or Keys.RightApplication)
-            {
+                or Keys.LeftApplication or Keys.RightApplication) {
                 return;
             }
 
-            if (newHotkey == currentHotkey) {
-                Close();
+            OnHotkeyDown(currentHotkey, newHotkey);
+        };
+        Content.TextInput += (_, e) => {
+            if (e.Text.Length != 1) {
                 return;
             }
 
-            // Avoid conflicts with other hotkeys
-            var conflictingKeyBinds = keyBindings.Where(pair => pair.Value == newHotkey).Select(pair => pair.Key).ToArray();
-            var conflictingSnippets = snippets.Where(snippet => snippet.Hotkey == newHotkey).ToArray();
-
-            if (conflictingKeyBinds.Any() || conflictingSnippets.Any()) {
-                var msg = new StringBuilder();
-                msg.AppendLine($"This hotkey ({newHotkey.ToShortcutString()}) is already used for other key bindings / snippets!");
-                if (conflictingKeyBinds.Any()) {
-                    msg.AppendLine("The following key bindings already use this hotkey:");
-                    foreach (var conflict in conflictingKeyBinds) {
-                        msg.AppendLine($"    - {conflict.GetName().Replace("&", string.Empty)}");
-                    }
-                    msg.AppendLine(string.Empty);
-                }
-                if (conflictingSnippets.Any()) {
-                    msg.AppendLine("The following snippets already use this hotkey:");
-                    foreach (var conflict in conflictingSnippets) {
-                        var lines = conflict.Insert.ReplaceLineEndings(Document.NewLine.ToString()).Split(Document.NewLine);
-                        var shortcut = !string.IsNullOrWhiteSpace(conflict.Shortcut) ? $"'{conflict.Shortcut}' = " : "";
-                        var insert = lines[0] + (lines.Length > 1 ? "..." : string.Empty);
-                        msg.AppendLine($"    - {shortcut}'{insert}'");
-                    }
-                    msg.AppendLine(string.Empty);
-                }
-                msg.AppendLine("Are you sure you want to use this hotkey?");
-
-                var confirm = MessageBox.Show(msg.ToString(), MessageBoxButtons.YesNo, MessageBoxType.Question, MessageBoxDefaultButton.Yes);
-                if (confirm != DialogResult.Yes) {
-                    return;
-                }
-            }
-
-            if (newHotkey == (Application.Instance.CommonModifier | Keys.Escape)) {
-                Close(Hotkey.None);
-            } else {
-                Close(newHotkey);
-            }
+            var newHotkey = Hotkey.Char(e.Text[0]);
+            OnHotkeyDown(currentHotkey, newHotkey);
         };
 
         Studio.RegisterDialog(this, ParentWindow);
+    }
+
+    private void OnHotkeyDown(Hotkey currentHotkey, Hotkey newHotkey) {
+        if (newHotkey == currentHotkey) {
+            Close();
+            return;
+        }
+
+        // Avoid conflicts with other hotkeys
+        var conflictingKeyBinds = keyBindings.Where(pair => pair.Value == newHotkey).Select(pair => pair.Key).ToArray();
+        var conflictingSnippets = snippets.Where(snippet => snippet.Hotkey == newHotkey).ToArray();
+
+        if (conflictingKeyBinds.Any() || conflictingSnippets.Any()) {
+            var msg = new StringBuilder();
+            msg.AppendLine($"This hotkey ({newHotkey.ToShortcutString()}) is already used for other key bindings / snippets!");
+            if (conflictingKeyBinds.Any()) {
+                msg.AppendLine("The following key bindings already use this hotkey:");
+                foreach (var conflict in conflictingKeyBinds) {
+                    msg.AppendLine($"    - {conflict.GetName().Replace("&", string.Empty)}");
+                }
+                msg.AppendLine(string.Empty);
+            }
+            if (conflictingSnippets.Any()) {
+                msg.AppendLine("The following snippets already use this hotkey:");
+                foreach (var conflict in conflictingSnippets) {
+                    var lines = conflict.Insert.ReplaceLineEndings(Document.NewLine.ToString()).Split(Document.NewLine);
+                    var shortcut = !string.IsNullOrWhiteSpace(conflict.Shortcut) ? $"'{conflict.Shortcut}' = " : "";
+                    var insert = lines[0] + (lines.Length > 1 ? "..." : string.Empty);
+                    msg.AppendLine($"    - {shortcut}'{insert}'");
+                }
+                msg.AppendLine(string.Empty);
+            }
+            msg.AppendLine("Are you sure you want to use this hotkey?");
+
+            var confirm = MessageBox.Show(msg.ToString(), MessageBoxButtons.YesNo, MessageBoxType.Question, MessageBoxDefaultButton.Yes);
+            if (confirm != DialogResult.Yes) {
+                return;
+            }
+        }
+
+        if (newHotkey == (Application.Instance.CommonModifier | Keys.Escape)) {
+            Close(Hotkey.None);
+        } else {
+            Close(newHotkey);
+        }
     }
 
     public static Hotkey Show(Window parent, Hotkey currentHotkey, Dictionary<MenuEntry, Hotkey>? keyBindings, List<Snippet>? snippets) {
