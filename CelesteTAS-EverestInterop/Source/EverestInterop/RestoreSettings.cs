7ffd16603467e16fe1bc8b24d5fbfe7b7f891819
psyGamer <psygamer21@gmail.com>
2025-04-11 21:53:24 +0200

    fix: Savestates causing modded bindings to break after restoring settings

--- CelesteTAS-EverestInterop/Source/EverestInterop/RestoreSettings.cs
+++ CelesteTAS-EverestInterop/Source/EverestInterop/RestoreSettings.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using Celeste;
 using Celeste.Mod;
+using Celeste.Mod.SpeedrunTool.SaveLoad;
 using Monocle;
 using System;
+using System.Runtime.CompilerServices;
 using TAS.Input.Commands;
 using TAS.ModInterop;
 using TAS.Module;
@@ -46,7 +48,17 @@ internal static class RestoreSettings {
                 continue;
             }
 
-            origModSettings.Add(module, module._Settings.ShallowClone());
+            // When using savestates, need to deep clone settings, to avoid issues with ButtonBindings breaking
+            if (SpeedrunToolInterop.Installed) {
+                origModSettings.Add(module, DeepClone(module._Settings));
+            } else {
+                origModSettings.Add(module, module._Settings.ShallowClone());
+            }
+            continue;
+
+            // Need separate method to avoid crash if SRT isn't installed
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            static T DeepClone<T>(T obj) => obj.DeepCloneShared();
         }
 
         origExtendedVariants.Clear();
