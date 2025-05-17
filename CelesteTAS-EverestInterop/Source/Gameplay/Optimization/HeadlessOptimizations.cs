6108875b050b802cb0a3b23849fc8c7e0de36c16
psyGamer <psygamer21@gmail.com>
2025-05-17 14:42:48 +0200

    perf(Headless): Prevent generation of tile grid

---
+++ CelesteTAS-EverestInterop/Source/Gameplay/Optimization/HeadlessOptimizations.cs
@@ -0,0 +1,35 @@
+using Celeste;
+using Celeste.Mod;
+using Monocle;
+using TAS.Module;
+using TAS.Tools;
+using TAS.Utils;
+
+namespace TAS.Gameplay.Optimization;
+
+/// Optimizations applied to the game, which are only applicable in headless mode
+/// Most simply involve not computing data, which is only visually required
+internal static class HeadlessOptimizations {
+
+    private static Autotiler.Generated stubbedTilemap = default;
+
+    [LoadContent]
+    private static void LoadContent() {
+        if (!Everest.Flags.IsHeadless && !SyncChecker.Active) {
+            return;
+        }
+
+        // Prevent tilemaps from being generated
+        stubbedTilemap = new Autotiler.Generated {
+            TileGrid = new TileGrid(8, 8, 1, 1),
+            SpriteOverlay = new AnimatedTiles(1, 1, GFX.AnimatedTilesBank)
+        };
+
+        typeof(Autotiler)
+            .GetMethodInfo(nameof(Autotiler.Generate))!
+            .IlHook((cursor, _) => {
+                cursor.EmitLdsfld(typeof(HeadlessOptimizations).GetFieldInfo(nameof(stubbedTilemap))!);
+                cursor.EmitRet();
+            });
+    }
+}
