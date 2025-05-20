d777fa5d9b365759cbd45c54ac4c5c50b7f5a739
Lozen <865466388@qq.com>
2025-05-21 00:12:05 +0800

    leave a comment for future development

--- CelesteTAS-EverestInterop/Source/ModInterop/TasSpeedrunToolInterop.cs
+++ CelesteTAS-EverestInterop/Source/ModInterop/TasSpeedrunToolInterop.cs
@@ -1,10 +1,21 @@
 ﻿using MonoMod.ModInterop;
 using TAS.Module;
+using Celeste.Mod;
 using System;
+using System.Linq;
 
 namespace TAS.ModInterop;
 
 internal static class TasSpeedrunToolInterop {
+    public static bool MultipleSaveSlotsSupported() {
+        // note TasSpeedrunToolInterop appears in SRT v3.24.4
+        // in v3.24.4, everything is same as before, except that this class is added for compatibility issue
+        // after v3.25.0, SRT supports multiple saveslots
+        if (Everest.Modules.FirstOrDefault(module => module.Metadata.Name == "SpeedrunTool") is { } srtModule) {
+            return srtModule.Metadata.Version >= new Version(3, 25, 0);
+        }
+        return false;
+    }
 
     public static bool Installed = false;
 
