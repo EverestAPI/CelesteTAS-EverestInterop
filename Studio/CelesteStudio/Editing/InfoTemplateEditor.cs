4c9fe6190588f00a721616e909a670761d6cef07
psyGamer <psygamer21@gmail.com>
2025-11-01 13:24:31 +0100

    feat(Studio): Support info-template tables and attempt to auto-close syntax pairs

--- Studio/CelesteStudio/Editing/InfoTemplateEditor.cs
+++ Studio/CelesteStudio/Editing/InfoTemplateEditor.cs
@@ -11,12 +11,98 @@ public class InfoTemplateEditor : TextEditor {
         autoCompleteMenu = new InfoTemplateAutoCompleteMenu(this);
     }
 
-    private enum LineMode { Regular, TargetQuery, Lua }
+    protected override void OnTextInput(TextInputEventArgs e) {
+        if (e.Text.Length == 0 || char.IsControl(e.Text[0])) {
+            return;
+        }
+
+        Document.Caret = ClampCaret(Document.Caret);
+
+        // Attempt to auto-close special syntax pairs
+        if (e.Text == "{") {
+            string line = Document.Lines[Document.Caret.Row];
+            int nextOpen  = line.IndexOf('{', startIndex: Document.Caret.Col);
+            int nextClose = line.IndexOf('}', startIndex: Document.Caret.Col);
+
+            if (nextClose == -1 || nextClose > nextOpen) {
+                using var __ = Document.Update();
+
+                if (!Document.Selection.Empty) {
+                    RemoveRange(Document.Selection.Min, Document.Selection.Max);
+                    Document.Caret = Document.Selection.Min;
+                    Document.Selection.Clear();
+                }
+
+                Document.Insert("{}");
+                DesiredVisualCol = Document.Caret.Col -= 1;
+
+                Recalc();
+                autoCompleteMenu?.Refresh();
+                return;
+            }
+        } else if (e.Text == "[") {
+            string line = Document.Lines[Document.Caret.Row];
+            int nextOpen  = line.IndexOf("[[", startIndex: Document.Caret.Col, StringComparison.Ordinal);
+            int nextClose = line.IndexOf("]]", startIndex: Document.Caret.Col, StringComparison.Ordinal);
+            bool nextToExisting = Document.Caret.Col > 0 && line[Document.Caret.Col - 1] == '[' || Document.Caret.Col + 1 < line.Length && line[Document.Caret.Col + 1] == '[';
+
+            if ((nextClose == -1 || nextClose > nextOpen) && !nextToExisting) {
+                using var __ = Document.Update();
+
+                if (!Document.Selection.Empty) {
+                    RemoveRange(Document.Selection.Min, Document.Selection.Max);
+                    Document.Caret = Document.Selection.Min;
+                    Document.Selection.Clear();
+                }
+
+                Document.Insert("[[]]");
+                DesiredVisualCol = Document.Caret.Col -= 2;
+
+                Recalc();
+                autoCompleteMenu?.Refresh();
+                return;
+            }
+        } else if (e.Text == "|") {
+            string line = Document.Lines[Document.Caret.Row];
+            bool nextToExisting = Document.Caret.Col > 0 && line[Document.Caret.Col - 1] == '|' || Document.Caret.Col + 1 < line.Length && line[Document.Caret.Col + 1] == '|';
+
+            int amountBefore = 0;
+            for (int i = Document.Caret.Col - 2; i >= 0; i--) {
+                if (line[i] == '|' && line[i + 1] == '|') {
+                    amountBefore++;
+                    i--;
+                }
+            }
+
+            if (amountBefore % 2 == 0 && !nextToExisting) {
+                using var __ = Document.Update();
+
+                if (!Document.Selection.Empty) {
+                    RemoveRange(Document.Selection.Min, Document.Selection.Max);
+                    Document.Caret = Document.Selection.Min;
+                    Document.Selection.Clear();
+                }
+
+                Document.Insert("||||");
+                DesiredVisualCol = Document.Caret.Col -= 2;
+
+                Recalc();
+                autoCompleteMenu?.Refresh();
+                return;
+            }
+        }
+
+        base.OnTextInput(e);
+    }
+
+    private enum LineMode { Regular, TargetQuery, Lua, Table }
     protected override void DrawLine(SKCanvas canvas, string line, float x, float y) {
         var lineSpan = line.AsSpan();
 
         var mode = LineMode.Regular;
         int modeStart = 0;
+        bool inTable = false;
+        int tableStart = 0;
 
         for (int i = 0; i < lineSpan.Length; i++) {
             char c = lineSpan[i];
@@ -29,10 +115,17 @@ public class InfoTemplateEditor : TextEditor {
                 nextMode = LineMode.TargetQuery;
             } else if (c == '[' && n == '[') {
                 nextMode = LineMode.Lua;
+            } else if (!inTable && c == '|' && n == '|') {
+                nextMode = LineMode.Table;
+                inTable = true;
+                tableStart = i;
             } else if (mode == LineMode.TargetQuery && p == '}') {
-                nextMode = LineMode.Regular;
+                nextMode = inTable ? LineMode.Table :LineMode.Regular;
             } else if (mode == LineMode.Lua && p == ']' && pp == ']') {
+                nextMode = inTable ? LineMode.Table : LineMode.Regular;
+            } else if (inTable && tableStart + "||||".Length <= i && p == '|' && pp == '|') {
                 nextMode = LineMode.Regular;
+                inTable = false;
             }
 
             if (nextMode != mode) {
@@ -41,6 +134,7 @@ public class InfoTemplateEditor : TextEditor {
                     LineMode.Regular => Settings.Instance.Theme.StatusFgPaint,
                     LineMode.TargetQuery => Settings.Instance.Theme.CommandPaint.ForegroundColor,
                     LineMode.Lua => Settings.Instance.Theme.ActionPaint.ForegroundColor,
+                    LineMode.Table => Settings.Instance.Theme.FramePaint.ForegroundColor,
                     _ => throw new ArgumentOutOfRangeException()
                 };
 
@@ -55,6 +149,7 @@ public class InfoTemplateEditor : TextEditor {
             LineMode.Regular => Settings.Instance.Theme.StatusFgPaint,
             LineMode.TargetQuery => Settings.Instance.Theme.CommandPaint.ForegroundColor,
             LineMode.Lua => Settings.Instance.Theme.ActionPaint.ForegroundColor,
+            LineMode.Table => Settings.Instance.Theme.FramePaint.ForegroundColor,
             _ => throw new ArgumentOutOfRangeException()
         };
         canvas.DrawText(lastSpan, x + modeStart * Font.CharWidth(), y + Font.Offset(), Font, lastPaint);
