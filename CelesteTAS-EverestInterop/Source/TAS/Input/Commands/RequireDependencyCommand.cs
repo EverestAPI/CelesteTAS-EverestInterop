d548bce1e9d4c2e4a0e2d7f8e0116d4817e14e57
psyGamer <psygamer21@gmail.com>
2025-11-08 17:25:55 +0100

    refactor: Rework implementation of 'RequireDependency' command
    - Check during parse-time instead of run-time
    - Show confirmation menu, both in-game and overworld
    - Directly interact with OuiDepndencyDownloader

--- CelesteTAS-EverestInterop/Source/TAS/Input/Commands/RequireDependencyCommand.cs
+++ CelesteTAS-EverestInterop/Source/TAS/Input/Commands/RequireDependencyCommand.cs
@@ -5,168 +5,312 @@ using Microsoft.Xna.Framework;
 using Monocle;
 using StudioCommunication;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using TAS.Module;
+using TAS.Playback;
+using TAS.Utils;
 
 namespace TAS.Input.Commands;
 
 internal static class RequireDependencyCommand {
 
-    internal const string CommandName = "RequireDependency";
+    public const string CommandName = "RequireDependency";
     private class Meta : ITasCommandMeta {
         public string Insert => $"{CommandName}{CommandInfo.Separator}[0;Mod Name]{CommandInfo.Separator}[1;(Version)]";
 
         public bool HasArguments => true;
     }
 
-    // "RequireDependency, StrawberryJam2021",
-    // "RequireDependency, StrawberryJam2021, 1.0.9"
+    private static readonly Dictionary<string, Version?> missingDependencies = new();
 
-
-    [TasCommand(CommandName, ExecuteTiming = ExecuteTiming.Runtime, LegalInFullGame = false, CalcChecksum = false, MetaDataProvider = typeof(Meta))]
-    private static void RequireDependency(CommandLine commandLine, int studioLine, string filePath, int fileLine) {
-        // if not found, then we goto mod options menu, and let MODOPTIONS_COREMODULE_DOWNLOADDEPS appear, ask Everest to download the dependency as described in the tas file
-        // it seems this needs Everest.Flags.SupportRuntimeMods = true
-        string[] args = commandLine.Arguments;
-        if (args.Length == 0) {
+    [ParseFileEnd]
+    private static void ParseFileEnd() {
+        if (missingDependencies.IsEmpty()) {
             return;
         }
 
-        EverestModuleMetadata dependency = new() {
-            Name = args[0],
-            VersionString = args.Length > 1 ? args[1] : "0.0.1"
-        };
+        switch (Engine.Scene) {
+            case Level level:
+                ShowTextMenu(level);
+                break;
+            case Overworld overworld:
+                var oui = new OuiInstallDependenciesConfirmation();
+                overworld.Add(oui);
+                overworld.routineEntity.Add(new Coroutine(overworld.GotoRoutine(oui)));
+                break;
 
-        if (Everest.Modules.Where(module => module.Metadata.Name == dependency.Name) is { } matches && matches.Any()) {
-            if (args.Length == 1) {
-                return;
-            }
-            foreach (EverestModule installed in matches) {
-                if (Everest.Loader.VersionSatisfiesDependency(dependency.Version, installed.Metadata.Version)) {
-                    return;
+            default: {
+                foreach ((string name, var version) in missingDependencies) {
+                    if (version == null) {
+                        PopupToast.ShowWithColor($"{name} is not loaded.", Color.Red);
+                    } else {
+                        PopupToast.ShowWithColor($"{name} v{version.ToString(3)} is not loaded.", Color.Red);
+                    }
                 }
+                break;
             }
         }
 
-        string dependencyNameWithVersion = args.Length > 1 ? $"{dependency.Name} {args[1]}" : dependency.Name;
-        InstallDependencyUI.Create(dependency, dependencyNameWithVersion);
+        foreach ((string name, var version) in missingDependencies) {
+            if (version == null) {
+                $"{name} is not loaded.".Log(LogLevel.Error);
+            } else {
+                $"{name} v{version.ToString(3)} is not loaded.".Log(LogLevel.Error);
+            }
+        }
 
-        AbortTas($"{dependencyNameWithVersion} is not loaded.", true);
+        Manager.DisableRunLater();
     }
 
-    internal static class InstallDependencyUI {
+    [ClearInputs]
+    private static void Clear() {
+        missingDependencies.Clear();
+    }
 
-        // there will be some issue when RequireDependency wants to install and Console load command succeeds in same frame
-        // luckily this only happens when their parameters are not the same map
+    // "RequireDependency, ModName"
+    // "RequireDependency, ModName, ModVersion"
+    [TasCommand(CommandName, ExecuteTiming = ExecuteTiming.Parse, CalcChecksum = true, MetaDataProvider = typeof(Meta))]
+    private static void RequireDependency(CommandLine commandLine, int studioLine, string filePath, int fileLine) {
+        var controller = Manager.Controller;
+        var src = new SourceLocation(filePath, fileLine, studioLine);
 
-        private const string dummyName = "CelesteTAS - DependencyRequestor";
+        string[] args = commandLine.Arguments;
+        if (args.Length == 0) {
+            controller.ReportError(src, "Expected 'Mod Name' argument");
+            return;
+        }
 
-        private static int dummyCount = 0;
-        internal static void Create(EverestModuleMetadata dependency, string modName) {
-            if (Engine.Scene is not Level level) {
-                GotoInstall(dependency);
+        string modName = args[0];
+        foreach (var installed in Everest.Modules.Where(module => module.Metadata.Name == modName)) {
+            if (args.Length == 1) {
+                return; // Accept any version
+            }
+
+            if (Everest.Loader.VersionSatisfiesDependency(new Version(args[1]), installed.Metadata.Version)) {
                 return;
             }
-
-            level.wasPaused = true;
-            if (!level.Paused) {
-                level.StartPauseEffects();
-            }
-            else if (level.Entities.FindFirst<TextMenu>() is TextMenu textMenu) {
-                textMenu.Close();
-            }
-            level.Paused = true;
-            level.PauseMainMenuOpen = false;
-            TextMenu menu = new TextMenu {
-                Justify = new Vector2(0.5f, 0.8f)
-            };
-            menu.OnESC = menu.OnCancel = menu.OnPause = () => {
-                menu.RemoveSelf();
-                level.Paused = false;
-                Audio.Play("event:/ui/game/unpause");
-                level.unpauseTimer = 0.15f;
-            };
-            menu.Add(new TextMenu.Header("Install Dependency".ToDialogText()));
-            menu.Add(new TextMenu.SubHeader(modName, false));
-            menu.Add(new TextMenuExt.SubHeaderExt("") { HeightExtra = 30f });
-            menu.Add(new TextMenu.Button(Dialog.Clean("menu_return_continue")).Pressed(() => {
-                GotoInstall(dependency);
-            }));
-            menu.Add(new TextMenu.Button(Dialog.Clean("menu_return_cancel")).Pressed(menu.OnCancel));
-            level.Add(menu);
         }
 
-        internal static void GotoInstall(EverestModuleMetadata metadata) {
-            if (ToInstalledMods.TryGetValue(metadata.Name, out Version version) && metadata.Version <= version) {
-                Engine.Scene.OnEndOfFrame += GotoOuiModOptions;
-                return;
+        // No matching mod found
+        missingDependencies[modName] = args.Length == 1 ? null : new Version(args[1]);
+    }
+
+    private static TextMenu CreateTextMenu(Action<TextMenu> onConfirm, Action<TextMenu> onCancel) {
+        var menu = new TextMenu();
+        menu.OnESC = menu.OnCancel = menu.OnPause = () => onCancel(menu);
+
+        menu.Add(new TextMenu.Header("MISSING_DEPENDENCIES".ToDialogText()));
+        menu.Add(new TextMenu.SubHeader("INSTALL_DEPENDENCIES".ToDialogText(), topPadding: false));
+        menu.Add(new TextMenuExt.SubHeaderExt("") { HeightExtra = 10.0f });
+
+        int count = 0;
+        const int maxEntries = 6;
+        foreach ((string name, var version) in missingDependencies) {
+            if (count == maxEntries && missingDependencies.Count > maxEntries) {
+                string text = Dialog.Clean("REMAINING_DEPENDENCIES".ToDialogText())
+                    .Replace("((left))", (missingDependencies.Count - (maxEntries - 1)).ToString());
+                menu.Add(new TextMenu.Button(text) { Selectable = false });
+                break;
             }
 
-            ToInstalledMods[metadata.Name] = metadata.Version;
+            menu.Add(new TextMenu.Button(version == null ? name : $"{name} v{version.ToString(3)}") { Selectable = false });
+            count++;
+        }
+        menu.Add(new TextMenuExt.SubHeaderExt("") { HeightExtra = 30.0f });
 
-            // Everest will not load it if its name already exists, so we make sure its name is unique
-            dummyCount++;
-            EverestModuleMetadata dummy = new() {
-                Name = $"{dummyName}({dummyCount})",
-                Dependencies = [metadata],
-                VersionString = "1.0.0"
-            };
-            Everest.Loader.LoadModDelayed(dummy, null);
-            Engine.Scene.OnEndOfFrame += GotoOuiModOptions;
+        menu.Add(new TextMenu.Button(Dialog.Clean("menu_return_continue")).Pressed(() => onConfirm(menu)));
+        menu.Add(new TextMenu.Button(Dialog.Clean("menu_return_cancel")).Pressed(menu.OnCancel));
+
+        return menu;
+    }
+
+    private static void ShowTextMenu(Level level) {
+        var dependencies = missingDependencies.Select(entry => new EverestModuleMetadata {
+            Name = entry.Key,
+            Version = entry.Value ?? new Version(0, 0, 1), // A "0.0.*" version is special cased
+        }).ToList();
+
+        level.wasPaused = true;
+        if (!level.Paused) {
+            level.StartPauseEffects();
+        } else if (level.Tracker.GetEntityTrackIfNeeded<TextMenu>() is { } textMenu) {
+            textMenu.Close();
         }
 
-        internal static void GotoOuiModOptions() {
-            Engine.Scene = OverworldLoaderExt.FastGoto<OuiModOptions>();
+        level.Paused = true;
+        level.PauseMainMenuOpen = false;
+        var menu = CreateTextMenu(menu => {
+            // Adjusted vanilla 'Save & Quit' logic
+            menu.Focused = false;
+#pragma warning disable CS0618 // Type or member is obsolete
+            Engine.TimeRate = 1f;
+#pragma warning restore CS0618 // Type or member is obsolete
+            Audio.SetMusic(null);
+            Audio.BusStopAll(Buses.GAMEPLAY, immediate: true);
+
+            level.Session.InArea = true;
+            level.Session.Deaths++;
+            level.Session.DeathsInCurrentLevel++;
+            SaveData.Instance.AddDeath(level.Session.Area);
+
+            level.DoScreenWipe(wipeIn: false, () => {
+                Engine.Scene = new LevelExitExt(LevelExit.Mode.SaveAndQuit, level.Session, level.HiresSnow) {
+                    OverworldLoaderOverride = new OverworldLoaderExt(Overworld.StartMode.MainMenu) {
+                        PostLoadAction = overworld => {
+                            // Setup mountain model
+                            overworld.Mountain.SnapCamera(-1, new MountainCamera(new Vector3(0f, 6.0f, 12.0f), MountainRenderer.RotateLookAt));
+                            overworld.Mountain.GotoRotationMode();
+                            overworld.Maddy.Hide();
+
+                            // Hide main menu
+                            if (overworld.Current is OuiMainMenu mainMenu) {
+                                mainMenu.mountainStartFront = false;
+                            }
+                            overworld.Current.Components.RemoveAll<Coroutine>();
+                            overworld.Current.Focused = overworld.Current.Visible = false;
+
+                            // Show dependency installer
+                            OuiDependencyDownloader.MissingDependencies = dependencies;
+
+                            var oui = overworld.GetUI<OuiDependencyDownloader>();
+                            oui.Visible = true;
+                            overworld.Last = overworld.Current = oui;
+                            overworld.routineEntity.Add(new Coroutine(EnterRoutine(oui)));
+                        }
+                    }
+                };
+            }, hiresSnow: true);
+
+            foreach (var component in level.Tracker.GetComponents<LevelEndingHook>()) {
+                ((LevelEndingHook) component).OnEnd?.Invoke();
+            }
+        }, menu => {
+            menu.RemoveSelf();
+            level.Paused = false;
+            level.unpauseTimer = 0.15f;
+            Audio.Play(SFX.ui_game_unpause);
+        });
+
+        level.Add(menu);
+
+        static IEnumerator EnterRoutine(Oui target) {
+            var enumerator = target.Enter(null);
+            while (enumerator.MoveNext()) {
+                yield return enumerator.Current;
+            }
+
+            target.Focused = true;
+        }
+    }
+
+    private class OuiInstallDependenciesConfirmation : Oui {
+        private const float onScreenX = Celeste.Celeste.TargetWidth / 2.0f;
+        private const float offScreenX = onScreenX + Celeste.Celeste.TargetWidth;
+
+        private readonly TextMenu menu;
+        private float alpha = 0.0f;
+
+        private Oui? previousOui;
+
+        public OuiInstallDependenciesConfirmation() {
+            var dependencies = missingDependencies.Select(entry => new EverestModuleMetadata {
+                Name = entry.Key,
+                Version = entry.Value ?? new Version(0, 0, 1), // A "0.0.*" version is special cased
+            }).ToList();
+
+            menu = CreateTextMenu(_ => {
+                OuiDependencyDownloader.MissingDependencies = dependencies;
+                Overworld.Goto<OuiDependencyDownloader>();
+            }, _ => {
+                // Return to previous
+                Audio.Play(SFX.ui_main_button_back);
+                if (previousOui != null) {
+                    Overworld.routineEntity.Add(new Coroutine(Overworld.GotoRoutine(previousOui)));
+                } else {
+                    Overworld.Goto<OuiMainMenu>();
+                }
+            });
         }
 
-        private static readonly Dictionary<string, Version> ToInstalledMods = new();
-    }
-}
+        public override IEnumerator Enter(Oui from) {
+            previousOui = from;
 
-internal class OverworldLoaderExt : OverworldLoader {
+            Scene.Add(menu);
 
-    public Action<Overworld> overworldFirstAction;
-    public OverworldLoaderExt(Overworld.StartMode startMode, HiresSnow snow = null) : base(startMode, snow) {
-        Snow = null;
-        fadeIn = false;
-    }
+            menu.Visible = Visible = true;
+            menu.Focused = false;
 
-    public static OverworldLoaderExt FastGoto<T>() where T : Oui {
-        return new OverworldLoaderExt(Overworld.StartMode.MainMenu, null).SetOverworldAction(x => x.Goto<T>());
-    }
+            for (float p = 0.0f; p < 1.0f; p += Engine.DeltaTime * 4.0f) {
+                menu.X = offScreenX - Celeste.Celeste.TargetWidth*Ease.CubeOut(p);
+                alpha = Ease.CubeOut(p);
+                yield return null;
+            }
 
-    public override void Begin() {
-        Add(new HudRenderer());
-        /*
-        Add(Snow);
-        if (fadeIn) {
-            ScreenWipe.WipeColor = Color.Black;
-            new FadeWipe(this, wipeIn: true);
+            menu.Focused = true;
         }
-        */
-        base.RendererList.UpdateLists();
-        Session session = null;
-        if (SaveData.Instance != null) {
-            session = SaveData.Instance.CurrentSession_Safe;
+        public override IEnumerator Leave(Oui next) {
+            Audio.Play(SFX.ui_main_whoosh_large_out);
+            menu.Focused = false;
+
+            for (float p = 0.0f; p < 1.0f; p += Engine.DeltaTime * 4.0f) {
+                menu.X = onScreenX + Celeste.Celeste.TargetWidth*Ease.CubeIn(p);
+                alpha = 1.0f - Ease.CubeIn(p);
+                yield return null;
+            }
+
+            menu.Visible = Visible = false;
+            menu.RemoveSelf();
+            RemoveSelf();
         }
-        Entity entity = new Entity {
-            new Coroutine(Routine(session))
-        };
-        Add(entity);
-        activeThread = Thread.CurrentThread;
-        activeThread.Priority = ThreadPriority.Lowest;
-        RunThread.Start(LoadThreadExt, "OVERWORLD_LOADER_EXT", highPriority: true);
+
+        public override void Render() {
+            if (alpha > 0.0f) {
+                const float padding = 10.0f;
+                Draw.Rect(-padding, -padding, Celeste.Celeste.TargetWidth + padding*2.0f, Celeste.Celeste.TargetHeight + padding*2.0f, Color.Black * alpha * 0.4f);
+            }
+        }
     }
 
-    private void LoadThreadExt() {
-        base.LoadThread();
-        overworldFirstAction?.Invoke(overworld);
-    }
+    private class LevelExitExt(LevelExit.Mode mode, Session session, HiresSnow? snow = null) : LevelExit(mode, session, snow) {
+        public OverworldLoader? OverworldLoaderOverride;
 
-    public OverworldLoaderExt SetOverworldAction(Action<Overworld> action) {
-        overworldFirstAction = action;
-        return this;
+        public override void Begin() {
+            base.Begin();
+            if (OverworldLoaderOverride != null) {
+                overworldLoader = OverworldLoaderOverride;
+            }
+        }
+    }
+    private class OverworldLoaderExt : OverworldLoader {
+        public Action<Overworld>? PostLoadAction;
+        public OverworldLoaderExt(Overworld.StartMode startMode, HiresSnow? snow = null) : base(startMode, snow) {
+            Snow = null;
+            fadeIn = false;
+        }
+
+        public override void Begin() {
+            Add(new HudRenderer());
+
+            RendererList.UpdateLists();
+
+            Session? session = null;
+            if (SaveData.Instance != null) {
+                session = SaveData.Instance.CurrentSession_Safe;
+            }
+
+            Add([new Coroutine(Routine(session))]);
+
+            activeThread = Thread.CurrentThread;
+            activeThread.Priority = ThreadPriority.Lowest;
+            RunThread.Start(LoadThreadExt, "OVERWORLD_LOADER_EXT", highPriority: true);
+        }
+
+        private void LoadThreadExt() {
+            LoadThread();
+            PostLoadAction?.Invoke(overworld);
+        }
     }
 }
